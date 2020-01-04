c03da0e45cb95a6a9825348e4918c9ed6e39a90a
mill <ph.mill@gmail.com>
2020-01-04 15:59:56 +0100

    add postgres config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,9 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
